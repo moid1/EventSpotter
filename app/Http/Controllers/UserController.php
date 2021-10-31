9312b0db3fbc8af2e90a2db420e4ba76ce9ce34a
moid1 <43816034+moid1@users.noreply.github.com>
2021-10-31 13:28:19 +0500

    done with manaything

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Event;
 use App\Models\EventFeeds;
 use App\Models\Favrouite;
+use App\Models\Follower;
 use App\Models\Following;
 use App\Models\Likes;
 use App\Models\ProfileImage;
@@ -34,21 +35,45 @@ class UserController extends Controller
      */
     public function create()
     {
-        $upcomingEvents = Event::where('user_id', '!=', Auth::id())->where('event_date', '>=', date('Y-m-d'))->where('is_drafted', 0)->with(['eventPictures', 'user', 'comment','liveFeed'])->orderBy('created_at', 'DESC')->get();
+        $upcomingEvents = Event::where('user_id', '!=', Auth::id())->where('event_date', '>=', date('Y-m-d'))->where('is_drafted', 0)->with(['eventPictures', 'user', 'comment', 'liveFeed'])->orderBy('created_at', 'DESC')->get();
         $upcomingEvents = $upcomingEvents->except('user_id', Auth::id());
+        $new = null;
+        $followerss = Follower::where('user_id', Auth::id())->get()->pluck('follower_id');
+        $followingss = Following::where('user_id', Auth::id())->get()->pluck('following_id');
+        foreach ($upcomingEvents as $key => $value) {
+            $flag = false;
+            if ($value->is_public == 0) {
+                foreach ($followerss as $key => $follow) {
+                    if ($value->user_id == $follow) {
+                        $new[] = $value;
+                        $flag = true;
+                    }
+                }
+                if (!$flag) {
+                    foreach ($followingss as $key => $follow) {
+                        if ($value->user_id == $follow) {
+                            $new[] = $value;
+                        }
+                    }
+                }
+            } else {
+                $new[] = $value;
+            }
+        }
+        $upcomingEvents = $new;
         $user = User::where('id', Auth::id())->with(['followers', 'following'])->first();
         $nearEvents = array();
 
         foreach ($upcomingEvents as $key => $value) {
             $latLng = explode(',', $user->lat_lng); // user lat lng
-            if (is_array($latLng)) {
+           
+            if (count($latLng)>1) {
                 $mile = $this->distance($latLng[0], $latLng[1], $value->lat, $value->lng);
                 $fav = Favrouite::where('user_id', Auth::id())->where('event_id', $value->id)->first();
                 $liveFeed = EventFeeds::where('event_id', $value->id)->latest()->first();
                 $isFollowing = Following::where('user_id', Auth::id())->where('following_id', $value->user_id)->where('is_accepted', 1)->first();
                 $isLiked = Likes::where('user_id', Auth::id())->where('event_id', $value->id)->first();
                 $nearEvents[] = array('events' => $value, 'livefeed' => $liveFeed, 'km' => number_format($mile, 1), 'isFavroute' => $fav ? 1 : 0, 'Following' => $isFollowing ? 1 : 0, 'isLiked' => $isLiked ? 1 : 0);
-                
             }
         }
         // dd($nearEvents);
