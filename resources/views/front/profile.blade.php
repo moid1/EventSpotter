b487cd3c8050d76080fa2b13016c133694a41aaa
moid1 <43816034+moid1@users.noreply.github.com>
2021-11-20 03:32:46 +0500

    done

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -456,7 +456,8 @@
                             var img = event.events.event_pictures[0]
                                 .image_path.split('.').pop() == 'mp4' || event.events
                                 .event_pictures[0]
-                                .image_path.split('.').pop() == 'mov' ? '{{ asset("download.png") }}' :
+                                .image_path.split('.').pop() == 'mov' ?
+                                '{{ asset('download.png') }}' :
                                 window.location.origin + '/' + event.events.event_pictures[0]
                                 .image_path;
                             var url = "{{ url('eventDetails') }}" + "/" + event.events.id;
@@ -518,7 +519,11 @@
 
                         if (response.data.length > 0) {
                             response.data.forEach(function(event) {
-                                var img =
+                                var img = event.events.event_pictures[0]
+                                    .image_path.split('.').pop() == 'mp4' || event.events
+                                    .event_pictures[0]
+                                    .image_path.split('.').pop() == 'mov' ?
+                                    '{{ asset('download.png') }}' :
                                     window.location.origin + '/' + event.events.event_pictures[0]
                                     .image_path;
                                 var url = "{{ url('eventDetails') }}" + "/" + event.events.id;
@@ -578,7 +583,13 @@
 
                         if (response.data.length > 0) {
                             response.data.forEach(function(event) {
-                                var img = event.event_pictures[0].image_path;
+                                var img = event.events.event_pictures[0]
+                                    .image_path.split('.').pop() == 'mp4' || event.events
+                                    .event_pictures[0]
+                                    .image_path.split('.').pop() == 'mov' ?
+                                    '{{ asset('download.png') }}' :
+                                    window.location.origin + '/' + event.events.event_pictures[0]
+                                    .image_path;
                                 var url = "{{ url('eventDetails') }}" + "/" + event.events.id;
 
                                 $('#events').append("<a href=" + url +
