8436f95c5cc76a929f18922edd04b0632d1f4f89
moid1 <43816034+moid1@users.noreply.github.com>
2021-11-21 13:42:37 +0500

    done

--- resources/views/front/right_side.blade.php
+++ resources/views/front/right_side.blade.php
@@ -1,7 +1,6 @@
 <div class="col-md-3">
     <div class="notifications mx-auto">
         <p class="margin-left-20"> Notifications</p>
-        @if (count(\App\Models\Notifications::where('user_id',Auth::id())->get())>0)
         @foreach (\App\Models\Notifications::where('user_id', Auth::id())->take(5)->orderBy('created_at', 'DESC')->get()
     as $noti)
             <div class="d-flex align-items-center margin-5 side">
@@ -13,9 +12,7 @@
             </div>
             <hr>
         @endforeach
-        @else
-        <h6>No Notifications Available Right Now </h6>
-        @endif
+       
         <hr>
  
     </div>
