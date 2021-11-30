c3e96c66a7614ee786fbba8c7143be2c5ff35105
moid1 <43816034+moid1@users.noreply.github.com>
2021-12-01 00:43:20 +0500

    done

--- resources/views/front/right_side.blade.php
+++ resources/views/front/right_side.blade.php
@@ -3,17 +3,19 @@
         <p class="margin-left-20"> Notifications</p>
         @foreach (\App\Models\Notifications::where('user_id', Auth::id())->take(5)->orderBy('created_at', 'DESC')->get()
     as $noti)
-            <div class="d-flex align-items-center margin-5 side">
-                <div class="iconsBackgroundBox">
-                    <img src="{{ asset('assets/images/video.png') }}" alt="">
+            <a href="{{url($noti->route_name)}}">
+                <div class="d-flex align-items-center margin-5 side">
+                    <div class="iconsBackgroundBox">
+                        <img src="{{ asset('assets/images/video.png') }}" alt="">
+                    </div>
+                    <span class=" ml-2  notifications-primary-text"> {{ $noti->message }}</span>
+                    <span class="agoColor">{{ $noti->created_at->diffForHumans() }}</span>
                 </div>
-                <span class=" ml-2  notifications-primary-text"> {{ $noti->message }}</span>
-                <span class="agoColor">{{ $noti->created_at->diffForHumans() }}</span>
-            </div>
+            </a>
             <hr>
         @endforeach
-       
+
         <hr>
- 
+
     </div>
 </div>
