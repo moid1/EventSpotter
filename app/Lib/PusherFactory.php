ef5aa731231983976f3018e128d47af7f433aed7
moid1 <43816034+moid1@users.noreply.github.com>
2021-12-02 03:31:51 +0500

    done

--- app/Lib/PusherFactory.php
+++ app/Lib/PusherFactory.php
@@ -5,12 +5,13 @@ class PusherFactory
 {
     public static function make()
     {
+    //    dd(env("PUSHER_APP_CLUSTER"));
         return new Pusher(
             env("PUSHER_APP_KEY"), // public key
             env("PUSHER_APP_SECRET"), // Secret
             env("PUSHER_APP_ID"), // App_id
             array(
-                'cluster' => 'ap2', // Cluster
+                'cluster' => env("PUSHER_APP_CLUSTER"), // Cluster
                 'encrypted' => true,
             )
         );
