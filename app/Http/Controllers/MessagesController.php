7150374b94ad2bad159476933fec388ee8f1e782
moid1 <43816034+moid1@users.noreply.github.com>
2021-12-03 01:08:09 +0500

    done

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -94,7 +94,7 @@ class MessagesController extends Controller
         event(new MessageSent($message));
         // PusherFactory::make()->trigger('chat', 'order-filled', ['data' => 'MOI']);
 
-        PusherFactory::make()->trigger('chat', 'send', ['data' => $message]);
+        PusherFactory::make()->trigger('chat-message', 'send', ['data' => $message]);
        
 
         return response()->json(['state' => 1, 'data' => $message]);
