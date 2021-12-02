60269a0bd1c51c628692cc802d012dbe4355ebe5
moid1 <43816034+moid1@users.noreply.github.com>
2021-12-03 03:36:57 +0500

    done

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -63,7 +63,7 @@ class MessagesController extends Controller
      */
     public function postSendMessage(Request $request)
     {
-        return 'huhu';
+      return Auth::user();
         if (!$request->to_user || !$request->message) {
             return;
         }
