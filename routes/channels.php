a9bbcbced4cf7eb76d0affc4440b8462c5663719
moid1 <43816034+moid1@users.noreply.github.com>
2021-12-03 01:01:14 +0500

    done

--- routes/channels.php
+++ routes/channels.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-Broadcast::channel('chat-message.{userId}', function ($user, $userId) {
-    return (int) $user->id === (int) $userId;
+Broadcast::channel('chat-message', function ($user) {
+    return (int) $user->id === auth()->id();
 });
 
 Broadcast::channel('user.{userId}', function ($user, $userId) {
